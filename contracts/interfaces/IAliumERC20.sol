f04effab4227a7d25dec5a58bd543345952ca712
pavel bolhar <gfijrb@yandex.ua>
2022-11-01 20:03:50 +0200

    optimize IAliumERC20.sol with inherit IERC20

--- contracts/interfaces/IAliumERC20.sol
+++ contracts/interfaces/IAliumERC20.sol
@@ -1,32 +1,9 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity =0.8.15;
 
-interface IAliumERC20 {
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-    event Transfer(address indexed from, address indexed to, uint256 value);
+import "./IERC20.sol";
 
-    function name() external view returns (string memory);
-
-    function symbol() external view returns (string memory);
-
-    function decimals() external view returns (uint8);
-
-    function totalSupply() external view returns (uint256);
-
-    function balanceOf(address owner) external view returns (uint256);
-
-    function allowance(address owner, address spender) external view returns (uint256);
-
-    function approve(address spender, uint256 value) external returns (bool);
-
-    function transfer(address to, uint256 value) external returns (bool);
-
-    function transferFrom(
-        address from,
-        address to,
-        uint256 value
-    ) external returns (bool);
-
+interface IAliumERC20 is IERC20 {
     function DOMAIN_SEPARATOR() external view returns (bytes32);
 
     function PERMIT_TYPEHASH() external pure returns (bytes32);
